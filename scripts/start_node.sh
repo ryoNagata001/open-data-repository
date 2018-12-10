3acf43fc104ecc1ad58688e1f546d844e3591b68
ryo nagata <ryo.nagata.utokyo@gmail.com>
2018-12-10 23:27:40 +0900

    add comment

--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -3,4 +3,4 @@
 home=$GOPATH/src/open-data-repository
 
 tendermint init --home ${home}
-tendermint node --home ${home}
+tendermint node --home ${home} --consensus.create_empty_blocks=false #ここに他のパラメーターも加えれば、常時追加可能だろう
